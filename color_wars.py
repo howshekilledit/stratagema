be8e4843ee4a12824bf9c021273581c7c46d8fd7
howshekilledit <mgreen@allegheny.edu>
2024-07-26 19:48:53 -0400

    fixed neighbors problem holy shit

--- color_wars.py
+++ color_wars.py
@@ -6,8 +6,7 @@ import copy
 #RULES
 #1. The board is a grid of cells
 #2. Each cell is colored red, green, or blue
-#3. For each turn, player can  change  neighbors to the top, bottom, left, and right to its color, as long as the color is not already the same. It can swap positions with neighbors to the diagonal, as long as the color is not already the same. It can swap colors with any neighbor whose color is different, including diagonal. 
-
+#3. For each turn, the player can swap with an adjacent cell (up, down, left, right) or spread color to a diagonal cell (up-left, up-right, down-left, down-right).
 class intVector():
     def __init__(self, x, y):
         self.x = x
@@ -20,11 +19,11 @@ def setVal(vec, val, grid): # spec is a boolean that indicates if the update is 
     return grid
 
 class Board():
-    def __init__(self, width, height, cell_size):
+    def __init__(self, width, height, start = False):
         self.width = width
         self.height = height
-        self.cell_size = cell_size
-        self.grid = [[random.choice(['r', 'g', 'b'])  for _ in range(width)] for _ in range(height)]
+        if not start:
+            self.grid = [[random.choice(['c', 'm', 'y'])  for _ in range(width)] for _ in range(height)]
         self.pos = intVector(random.randint(0, width-1), random.randint(0, height-1))
         self.char = self.getVal(self.pos)
         self.goal = [[self.char] * self.width] * self.height #goal set <- change this line to TODO
@@ -36,16 +35,21 @@ class Board():
             for cell in row:
                 if cell.lower() == val:
                     count += 1
-    def print(self):
-        printGrid = copy.deepcopy(self.grid) # copy the grid without reference
-        printGrid = setVal(self.pos, self.char.upper(), printGrid)
-        
-        for row in printGrid:
-            print(''.join(row))
+    def print(self, grid  = False, pos = False, console = True):
+        if not grid:
+            grid = self.grid
+        if not pos:
+            pos = self.pos
+        printGrid = copy.deepcopy(grid) # copy the grid without reference
+        printGrid = setVal(pos, self.char.upper(), printGrid)
+        printGrid = '\n'.join([''.join(row) for row in printGrid])
+        if console:
+            print(printGrid)
+        return printGrid
     def neighbors(self, pos = False):
         if not pos:
             pos = self.pos
-        x, y = self.pos.x, self.pos.y
+        x, y = pos.x, pos.y
         neighbors = []
         # horizontal  vertical neighbors
         if x > 0:
@@ -92,29 +96,40 @@ class Board():
                 if self.getVal(neighbor, grid) != self.getVal(pos, grid):
                     state = copy.deepcopy(grid)
                     state = setVal(neighbor, self.getVal(pos, grid), state)
-                    moves.append({'name': 'spread_color', 'newpos': pos, 'state': state})
+                    moves.append({'name': f"spread color", "affected_pos": neighbor, 'new_pos': pos, 'state': state})
             # swap places to the top, bottom, left, or right 
             else:
                 neighborVal = self.getVal(neighbor, grid)
-                agentVal = self.getVal(pos, grid)
+                playerVal = self.getVal(pos, grid)
                 state = copy.deepcopy(grid)
-                state = setVal(neighbor, agentVal, state)
+                state = setVal(neighbor, playerVal, state)
                 state = setVal(pos, neighborVal, state)
-                moves.append({'name': 'swap_pos', 'newpos': neighbor, 'state': state})
+                moves.append({'name': "swap places", 'affected_pos': pos,  'new_pos': neighbor, 'state': state})
         return moves
     def makeRandomMove(self):
         moves = self.moves()
         move = random.choice(moves)
-        actionName = move[0]
-        neighborPos = move[1]
-        neighborVal = self.getVal(neighborPos)
-        agentVal = self.getVal(self.pos)
-        if actionName  == 'spread_color':
-            self.setVal(neighborPos, agentVal)
-        elif actionName  == 'swap_pos':
-            self.setVal(neighborPos, agentVal)
-            self.setVal(self.pos, neighborVal)
-            self.pos = neighborPos
+        self.grid = move['state']
+        self.pos = move['new_pos']
+    def decisionTree(self, parent = None, moves = False, tree = []):
+        if not moves:
+            moves = self.moves()
+            node = Node(state=self.grid, parent=parent, action=self.pos)
+            tree.append([self.grid])
+        grids = [move['state'] for move in moves]
+        # print grids out side by side by joining self.print() output for each
+        printGrids = [self.print(move['state'], move['new_pos'], False) for move in moves]
+        tree.append(printGrids)
+        printGrids = ' '.join([f"{grid[:2]}" for grid in printGrids])+ '\n' + ' '.join([f"{grid[-2:]}" for grid in printGrids])
+        
+        print(printGrids)
+        for move in moves:
+            grid = move['state']
+            pos = move['new_pos']
+            #tree.append(Node(state=grid, parent=parent, action=pos))
+            if self.goal != grid and len(tree) < 10:
+                self.decisionTree(grid, self.moves(pos, grid), tree)
+        return tree
     def solve_random(self):
         while self.grid != self.goal:
             self.makeRandomMove()
@@ -126,7 +141,7 @@ class Board():
     def set_frontier(self, frontier_type):
         if frontier_type == 'stack':
             self.frontier = StackFrontier()
-        elif frontier_type == 'queue':
+        if frontier_type == 'queue':
             self.frontier = QueueFrontier()
         self.frontier.add(Node(self.grid, None, None))
         self.explored = set()
@@ -136,23 +151,53 @@ class Board():
     def solve_dfs(self):
         self.set_frontier('stack')
         self.solve()
+    def solve_step(self): # solve one step at a time (type depends on the frontier)
+        if self.frontier.empty():
+            return None
+        node = self.frontier.remove()
+        self.removed = node.state
+        if node.state == self.goal:
+            print('Goal!')
+            return
+        self.explored.add(str(node.state))
+        for move in self.moves(node.action, node.state):
+            grid = move['state']
+            pos = move['new_pos']
+            if not self.frontier.contains_state(grid)  and str(grid) not in self.explored:
+                child = Node(state=grid, parent=node, action=pos)
+                self.frontier.add(child)
     def solve(self):
+        cost = 0
         while True:
             if self.frontier.empty():
+                print('No solution')
                 return None
             node = self.frontier.remove()
-            print(node.state)
+            self.removed = node.state
             if node.state == self.goal:
+                self.solution = []
+                while node.parent is not None:
+                    self.solution.append(node)
+                    node = node.parent
+                self.solution.reverse()
+                for node in self.solution:
+                    self.print(node.state, node.action)
+                    print('-'*self.width)
                 print('Goal!')
                 return
-            self.explored.add(str(node.state))
-            for move in self.moves(node.action, node.state):
+            self.explored.add(self.print(node.state, node.action, False))
+            if node.action is not None:
+                pos = node.action
+            else:
+                pos = self.pos
+            for move in self.moves(pos, node.state):
                 grid = move['state']
-                pos = move['newpos']
-                if not self.frontier.contains_state(grid)  and str(grid) not in self.explored:
+                pos = move['new_pos']
+                if not self.frontier.contains_state(grid) and self.print(grid, pos, False) not in self.explored:
                     child = Node(state=grid, parent=node, action=pos)
                     self.frontier.add(child)
 if __name__ == '__main__':
-    board = Board(2, 2, 100)
+    board = Board(2, 2)
     board.print()
+    print('-'*board.width)
     board.solve_dfs()
--- color_wars_p5.py
+++ color_wars_p5.py
@@ -1,32 +1,67 @@
 from p5 import *
 from color_wars import *
 
-class BoardP5(Board):
-        # display the board in p5
-    def display(self):
-        colors = {'r': Color(255, 0, 0), 'g': Color(0, 255, 0), 'b': Color(0, 0, 255)}
-        for y, row in enumerate(self.grid):
+class BoardP5(Board): #extends color wars board for visualization in p5
+    #add cell size argument to inherited __init__ method
+    def __init__(self, width, height, cell_size):
+        super().__init__(width, height)
+        self.cell_size = cell_size
+        self.pos = intVector(0, 0)
+    # display the board in p5
+    def display(self, grid = None, s = None, origin = Vector(0, 0)):
+        pos = intVector(0, 0)
+        if grid is None:
+            grid = self.grid
+            pos = self.pos
+        if s is None:
+            s = self.cell_size
+        colors = {'r': Color(255, 0, 0), 'g': Color(0, 255, 0), 'b': Color(0, 0, 255), 'c': Color(0, 255, 255), 'm': Color(255, 0, 255), 'y': Color(255, 255, 0), 'w': Color(255), 'k': Color(0)}
+        x_off, y_off = origin.x, origin.y
+        # if grid is string
+        print(grid)
+        if isinstance(grid, str):
+            grid = [[cell for cell in row] for row in grid.split('\n')]
+            
+            # find the position of the player
+            for y, row in enumerate(grid):
+                for x, cell in enumerate(row):
+                    if cell.isupper():
+                        pos = intVector(x, y)
+        for y, row in enumerate(grid):
             for x, cell in enumerate(row):
                 fill(colors[cell.lower()])
-                rect((x * self.cell_size, y * self.cell_size), self.cell_size, self.cell_size)
-                if self.pos.x == x and self.pos.y == y:
+                rect((x * s + x_off, y * s + y_off), s, s)
+                if pos.x == x and pos.y == y:
                     stroke(0)
-                    rect((x * self.cell_size, y * self.cell_size), self.cell_size, self.cell_size)
-                    ellipse((x * self.cell_size + self.cell_size // 2, y * self.cell_size + self.cell_size // 2), self.cell_size // 2, self.cell_size // 2)
+                    rect((x * s + x_off, y * s + y_off), s, s)
+                    ellipse((x * s + s // 2 + x_off, y * s + s // 2 + y_off), s // 2, s // 2)
                     noStroke()
+
 if __name__ == '__main__':
     board = BoardP5(2, 2, 100)
     board.print()
+    tree = board.decisionTree()
     def setup():
-        size(board.width * board.cell_size, board.height * board.cell_size)
+        size(1200, 1200)
+        background(255, 0, 0)
+        
     def draw():
-        background(255)
-        board.display()
-        if board.state == board.goal:
-            print('You won!')
-            text('You won!', 100, 100)
-            noLoop()
-        board.makeRandomMove()
-
+        for y, tier in enumerate(tree):
+            for x, grid in enumerate(tier):
+                print(grid)
+                board.display(grid, 10, Vector(x * 20, y * 20))
+    #def setup():
+    #    size(board.width * board.cell_size, board.height * board.cell_size)
+    #    board.display()
+    #    board.set_frontier('queue')
+#    def draw():
+#        background(255)
+#        if board.grid == board.goal:
+#            print('You won!')
+#            text('You won!', 100, 100)
+#            noLoop()
+#        #board.solve_step()
+#        board.makeRandomMove()
+#        board.display(grid = board.removed)
 
     run(frame_rate=1)
